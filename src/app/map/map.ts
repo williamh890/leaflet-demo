26a89d9afba7e929af97526964764f4318df6cd9
William Horn <wbhorn@alaska.edu>
2025-07-16 11:56:37 -0800

    Add gina overlay to map

--- src/app/map/map.ts
+++ src/app/map/map.ts
@@ -10,7 +10,7 @@ import * as L from 'leaflet';
 export class Map implements OnInit, AfterViewInit {
   private map!: L.Map
   markers: L.Marker[] = [
-    L.marker([23.7771, 90.3994]) // Dhaka, Bangladesh
+    L.marker([64.4156, -150.9315]) // Dhaka, Bangladesh
   ];
 
   constructor() { }
@@ -27,7 +27,16 @@ export class Map implements OnInit, AfterViewInit {
   private initMap() {
     const baseMapURl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
     this.map = L.map('map');
-    L.tileLayer(baseMapURl).addTo(this.map);
+
+    L.tileLayer(baseMapURl, {
+      minZoom: 5,
+      maxZoom: 15
+    }).addTo(this.map);
+
+    L.tileLayer('https://tiles.arcgis.com/tiles/2j08Y1PuezhQEfCz/arcgis/rest/services/ABoVE_2024_WIldfire_Exposure_v2/MapServer/tile/{z}/{y}/{x}', {
+      minZoom: 5,
+      maxZoom: 15
+    }).addTo(this.map);
   }
 
 
@@ -37,5 +46,6 @@ export class Map implements OnInit, AfterViewInit {
 
     // Fit the map into the boundary
     this.map.fitBounds(bounds);
+    this.map.setZoom(5)
   }
 }
