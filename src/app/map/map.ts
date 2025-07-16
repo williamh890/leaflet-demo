2298a46a2b9830871a6c24f6f1cd7a2838ea3a8a
William Horn <wbhorn@alaska.edu>
2025-07-16 11:39:08 -0800

    Fullscreen leaflet map

---
+++ src/app/map/map.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  imports: [],
+  templateUrl: './map.html',
+  styleUrl: './map.scss'
+})
+export class Map implements OnInit, AfterViewInit {
+  private map!: L.Map
+  markers: L.Marker[] = [
+    L.marker([23.7771, 90.3994]) // Dhaka, Bangladesh
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.initMap();
+    this.centerMap();
+  }
+
+
+  private initMap() {
+    const baseMapURl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    this.map = L.map('map');
+    L.tileLayer(baseMapURl).addTo(this.map);
+  }
+
+
+  private centerMap() {
+    // Create a boundary based on the markers
+    const bounds = L.latLngBounds(this.markers.map(marker => marker.getLatLng()));
+
+    // Fit the map into the boundary
+    this.map.fitBounds(bounds);
+  }
+}
